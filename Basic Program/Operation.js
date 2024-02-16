6b57fa6ca06e81c0ff11a49e91270de0c4cc1042
FaizAnsari04 <135959497+FaizAnsari04@users.noreply.github.com>
2024-02-16 08:33:59 +0000

    config basic

---
+++ Basic Program/Operation.js
@@ -0,0 +1,18 @@
+let num1=45;
+let num2=25;
+
+let sum=num1+num2;
+let sub=num1-num2;
+let mul=num1*num2;
+let div=num1/num2;
+
+console.log("Addition Of Two Number::" + sum);
+console.log("Subtraction Of Two Number::" + sub);
+console.log("Multipication Of Two Number::" + mul);
+console.log("Division Of Two Number::" + div);
+
+
+console.table(["Addition Of Two Number::" + sum,
+                "Subtraction Of Two Number::" + sub,
+                "Multipication Of Two Number::" + mul,
+                "Division Of Two Number::" + div]);
