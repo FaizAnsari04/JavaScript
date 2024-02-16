3b89da98076168749e92719fd18efba9ee6b890b
FaizAnsari04 <135959497+FaizAnsari04@users.noreply.github.com>
2024-02-16 09:34:23 +0000

    Basic Program

---
+++ Basic Program/Armstrong.js
@@ -0,0 +1,17 @@
+let num=154;
+let temp=num;
+let sum=0;
+while(temp)
+{
+    let digit=temp%10;
+    let value=digit*digit*digit;
+    sum+=value;
+    temp= parseInt(temp/10);
+}
+if(num==sum)
+{
+    console.log("Ha Hai Hai Bhai");
+}
+else{
+    console.log("Kasam Se Nahi Hai");
+}
