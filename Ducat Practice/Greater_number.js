7269eeb867a1865d31877593da8deaea4b5173ce
Faiz Ansari <135959497+FaizAnsari04@users.noreply.github.com>
2025-01-07 16:42:27 +0000

    Program

---
+++ Ducat Practice/Greater_number.js
@@ -0,0 +1,23 @@
+// var x = 10, y = 30, z = 60;
+// if (x > y &&x>z) {
+//     console.log(`${x} Is Greater`);
+
+// }
+// else if (y> x &&y>z) {
+//     console.log(`${y} Is Greater`);
+
+// }
+// else (z > y &&z>x) {
+//     console.log(`${z} Is Greater`);
+
+// }
+
+var x = 10, y = 30, z = 60;
+
+if (x > y && x > z) {
+    console.log(`${x} Is Greater`);
+} else if (y > x && y > z) {
+    console.log(`${y} Is Greater`);
+} else {
+    console.log(`${z} Is Greater`);
+}
