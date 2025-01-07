3bb555095b9992f05aa88fe0eb630e1e7a0e834e
Faiz Ansari <135959497+FaizAnsari04@users.noreply.github.com>
2025-01-07 16:23:21 +0000

    Program

---
+++ Ducat Practice/Checked_Weekday.js
@@ -0,0 +1,32 @@
+
+var op='2'
+if (op == '1') {
+    console.log(`${1}=Monday is Weekday`);
+}
+else if (op == '2') {
+    console.log(`${2}=Tuesday is Weekday`);
+}
+else if (op == '3') {
+    console.log(`${3}=Wednesday is Weekday`);
+}
+else if (op == '4') {
+    console.log(`${4}=Thrusday is Weekday`);
+}
+else if (op == '5') {
+    console.log(`${5}=Friday is Weekday`);
+    
+}
+//Weekends Days
+
+else if (op == '6') {
+    console.log(`${6}=Saturday is Weekend`);
+}
+else if (op == '7') {
+    console.log(`${7}=Sunday is Weekend`);
+}
+//Not a day
+
+else{
+    console.log("Not A Day");
+    
+}
